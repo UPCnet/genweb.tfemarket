bb600b7d996f6a6bf51e12062ce72f0ac62577bd
iago.lopez <iago.lopez@upcnet.es>
2018-04-06 08:01:43 +0200

    Preparar código mensajes

--- genweb/tfemarket/browser/events/change.py
+++ genweb/tfemarket/browser/events/change.py
@@ -2,8 +2,11 @@
 
 from datetime import date
 from datetime import datetime
+from plone import api
 from Products.CMFCore.utils import getToolByName
 
+from genweb.tfemarket.browser.events.messages import M1, M2, M3, M4, M5, M6
+from genweb.tfemarket.utils import getDegreeLiteralFromId
 from genweb.tfemarket.utils import sendMessage
 from genweb.tfemarket import _
 
@@ -14,45 +17,62 @@ def applicationChanged(application, event):
     """ If genweb.tfemarket.application change WF, sends email.
     """
 
+    lang = api.portal.get_current_language()
+    if lang not in ['ca', 'en', 'es']:
+        lang = 'en'
+
+    data = {
+        'student' : application.id,
+        'degree' : 'Titulació usuari',
+        'num' : application.getParentNode().offer_id,
+        'title' : application.getParentNode().title,
+        'linkApplication' : application.absolute_url(),
+        'linkOffer' : application.getParentNode().absolute_url(),
+        'linkMarket' : application.getParentNode().getParentNode().absolute_url(),
+        'firm' : 'TFE Mercat',
+    }
+
     fromMsg = toMsg = subject = msg = portalMsg = ''
     if event.transition is None:
+        # Añadir comprovación de la titulación, en caso de no tener enviar un
+        # portal message _(u'A1')
         fromMsg = 'Estudiant'
         toMsg = 'Professor'
         subject = 'Sol·licita'
-        msg = 'M1'
+        msg = M1[lang].format(**data)
     else:
         if event.transition.id == 'request':
             fromMsg = 'Estudiant'
             toMsg = 'Professor'
             subject = 'Sol·licita'
-            msg = 'M1'
+            msg = M1[lang].format(**data)
         elif event.transition.id == 'accept':
             fromMsg = 'Professor'
             toMsg = 'Estudiant'
             subject = 'Aceptada'
-            msg = 'M2'
+            msg = M2[lang].format(**data)
         elif event.transition.id == 'reject':
             fromMsg = 'Professor'
             toMsg = 'Estudiant'
             subject = 'Rebutjada'
-            msg = 'M3'
+            msg = M3[lang].format(**data)
         elif event.transition.id == 'confirm':
             fromMsg = 'Estudiant'
             toMsg = 'Professor'
             subject = 'Confirma'
-            msg = 'M4'
-            portalMsg = _(u'A3: Confirm offer')
+            msg = M4[lang].format(**data)
+            portalMsg = _(u'A3')
         elif event.transition.id == 'renounce':
             fromMsg = 'Estudiant'
             toMsg = 'Professor'
             subject = 'Renuncia'
-            msg = 'M5'
-            portalMsg = _(u'A4: Renounce offer')
+            msg = M5[lang].format(**data)
+            portalMsg = _(u'A4')
         elif event.transition.id == 'cancel':
             fromMsg = 'Estudiant'
             toMsg = 'Professor'
             subject = 'Cancela'
-            msg = 'M6'
+            msg = M6[lang].format(**data)
 
     if not fromMsg == '':
         sendMessage(application, fromMsg, toMsg, subject, msg)
@@ -64,6 +84,7 @@ def applicationChanged(application, event):
 def offerChanged(offer, event):
     """ If genweb.tfemarket.offer change WF, checks if expired.
     """
+
     if event.transition is not None:
         if event.transition.id == 'publicaalintranet' or event.transition.id == 'publicaloferta':
             wftool = getToolByName(offer, 'portal_workflow')
