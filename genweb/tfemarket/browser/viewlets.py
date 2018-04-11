ae7a527c7cbc87e0edb79bc3e481df64b6c54a51
iago.lopez <iago.lopez@upcnet.es>
2018-04-11 12:40:38 +0200

    Añadir icono con enlace a la info del mercado

--- genweb/tfemarket/browser/viewlets.py
+++ genweb/tfemarket/browser/viewlets.py
@@ -6,16 +6,44 @@ from zope.interface import Interface
 
 from plone import api
 from plone.app.layout.viewlets.interfaces import IAboveContent
+from plone.app.layout.viewlets.interfaces import IAboveContentTitle
 
 from genweb.tfemarket.interfaces import IGenwebTfemarketLayer
 from genweb.tfemarket.content.application import IApplication
 
 
+class infoTfemarket(grok.Viewlet):
+    grok.viewletmanager(IAboveContentTitle)
+    grok.context(Interface)
+    grok.name('genweb.tfemarket.info')
+    grok.template('infoTfemarket')
+    grok.layer(IGenwebTfemarketLayer)
+
+    def viewInfoTfeMarket(self):
+        validTypes = ['genweb.tfemarket.market',
+                      'genweb.tfemarket.offer',
+                      'genweb.tfemarket.application']
+        if self.context.portal_type in validTypes:
+            return True
+        return False
+
+    def urlInfo(self):
+        url = ""
+        portal_type = self.context.portal_type
+        if portal_type == 'genweb.tfemarket.market':
+            url = self.context.absolute_url() + "/info"
+        elif portal_type == 'genweb.tfemarket.offer':
+            url = self.context.getParentNode().absolute_url() + "/info"
+        elif portal_type == 'genweb.tfemarket.application':
+            url = self.context.getParentNode().getParentNode().absolute_url() + "/info"
+        return url
+
+
 class errorCreateApplication(grok.Viewlet):
     grok.viewletmanager(IAboveContent)
     grok.context(Interface)
+    grok.name('genweb.tfemarket.errorcreateapplication')
     grok.template('errorCreateApplication')
-    grok.require('cmf.ManagePortal')
     grok.layer(IGenwebTfemarketLayer)
 
     def getApplicationUrl(self):
