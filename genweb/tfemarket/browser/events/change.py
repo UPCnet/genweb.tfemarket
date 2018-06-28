1adbfcec5ddd1bacc017f98413022f555929ea89
Corina Riba <corina.riba@upcnet.es>
2018-06-28 11:03:35 +0200

    Integracion WS

--- genweb/tfemarket/browser/events/change.py
+++ genweb/tfemarket/browser/events/change.py
@@ -17,7 +17,12 @@ from genweb.tfemarket.browser.events.data.messages import M6
 from genweb.tfemarket.utils import checkOfferhasValidApplications
 from genweb.tfemarket.utils import sendMessage
 
+from zope.component import queryUtility
+from plone.registry.interfaces import IRegistry
+from genweb.tfemarket.controlpanel import IBUSSOASettings
+
 import transaction
+import requests
 
 
 def offerHasAnotherApplicationsPending(application):
@@ -155,3 +160,137 @@ def offerDeleted(offer, event):
         offer.plone_utils.addPortalMessage(_(u"The offer can't be deleted."), 'error')
         request = getRequest()
         request.response.redirect(offer.absolute_url())
+
+
+def offerRegistered(offer, event):
+    """ If genweb.tfemarket.offer change WF, checks if registered.
+    """
+
+    if event.transition is not None:
+        if event.transition.id in ['register', 'registralofertaintranet']:
+            registry = queryUtility(IRegistry)
+            bussoa_tool = registry.forInterface(IBUSSOASettings)
+            bussoa_url = bussoa_tool.bus_url
+            bussoa_user = bussoa_tool.bus_user
+            bussoa_pass = bussoa_tool.bus_password
+            bussoa_apikey = bussoa_tool.bus_apikey
+
+            # ONLY FOR TRY #####################
+
+            list_test = [{'id_prisma': '2866124', 'numDocument': '44522242S'},
+                         {'id_prisma': '2708530', 'numDocument': '47405847H'},
+                         {'id_prisma': '2708479', 'numDocument': '41747970A'},
+                         {'id_prisma': '2707723', 'numDocument': '18079004S'},
+                         {'id_prisma': '2706798', 'numDocument': '48085070M'},
+                         {'id_prisma': '2554111', 'numDocument': '53870113L'},
+                         {'id_prisma': '2553255', 'numDocument': '39934092C'},
+                         {'id_prisma': '2553249', 'numDocument': '53835514N'},
+                         {'id_prisma': '2553092', 'numDocument': '26063484E'},
+                         {'id_prisma': '2552221', 'numDocument': '21798723J'},
+                         {'id_prisma': '2551839', 'numDocument': 'X2756193B'},
+                         {'id_prisma': '2551740', 'numDocument': '73461145L'},
+                         {'id_prisma': '2551700', 'numDocument': '41578237X'},
+                         {'id_prisma': '2551388', 'numDocument': '47238061V'},
+                         {'id_prisma': '2550963', 'numDocument': '48031179A'},
+                         {'id_prisma': '2550923', 'numDocument': '79272419K'},
+                         {'id_prisma': '2550789', 'numDocument': '53316254T'},
+                         {'id_prisma': '2438885', 'numDocument': 'Y3346939Q'},
+                         {'id_prisma': '2433865', 'numDocument': '70077461A'},
+                         {'id_prisma': '2407950', 'numDocument': '05315643K'},
+                         {'id_prisma': '2407902', 'numDocument': '41574784F'},
+                         {'id_prisma': '2401711', 'numDocument': '47322096X'},
+                         {'id_prisma': '2401541', 'numDocument': '77624311R'},
+                         {'id_prisma': '2400827', 'numDocument': '17765920F'},
+                         {'id_prisma': '2400653', 'numDocument': '47835848B'},
+                         {'id_prisma': '2400648', 'numDocument': '48032008G'},
+                         {'id_prisma': '2400208', 'numDocument': '39936041Z'},
+                         {'id_prisma': '2400053', 'numDocument': '48037883Z'},
+                         {'id_prisma': '2399504', 'numDocument': '25624813F'},
+                         {'id_prisma': '2399333', 'numDocument': '45126841J'},
+                         {'id_prisma': '2399207', 'numDocument': '47332956Z'},
+                         {'id_prisma': '2399205', 'numDocument': '41597001Y'},
+                         {'id_prisma': '2399187', 'numDocument': 'Y3541903D'},
+                         {'id_prisma': '2398864', 'numDocument': '48037052B'},
+                         {'id_prisma': '2398761', 'numDocument': '43575236B'},
+                         {'id_prisma': '2398743', 'numDocument': '05458237S'},
+                         {'id_prisma': '2398335', 'numDocument': '53292318F'},
+                         {'id_prisma': '2398310', 'numDocument': '48090627L'},
+                         {'id_prisma': '2398303', 'numDocument': '46414025W'},
+                         {'id_prisma': '2398292', 'numDocument': '21780110F'},
+                         {'id_prisma': '2398291', 'numDocument': '53869317M'},
+                         {'id_prisma': '2398187', 'numDocument': '48019958Y'},
+                         {'id_prisma': '2398145', 'numDocument': '47838860X'},
+                         {'id_prisma': '2398058', 'numDocument': '48099790M'},
+                         {'id_prisma': '2397841', 'numDocument': '23878574C'},
+                         {'id_prisma': '2397674', 'numDocument': '47592922B'},
+                         {'id_prisma': '2397654', 'numDocument': '43460008J'},
+                         {'id_prisma': '2397644', 'numDocument': '48026090C'},
+                         {'id_prisma': '2397485', 'numDocument': '43570423M'},
+                         {'id_prisma': '2381463', 'numDocument': '47838895E'},
+                         {'id_prisma': '2201379', 'numDocument': '41586362Q'},
+                         {'id_prisma': '2195877', 'numDocument': '49876030R'},
+                         {'id_prisma': '2184958', 'numDocument': '24412328J'},
+                         {'id_prisma': '2174310', 'numDocument': '41549964G'},
+                         {'id_prisma': '2174137', 'numDocument': '41583105W'},
+                         {'id_prisma': '2173869', 'numDocument': '47962264L'},
+                         {'id_prisma': '2173315', 'numDocument': '53399800X'},
+                         {'id_prisma': '2167398', 'numDocument': '41577897S'},
+                         {'id_prisma': '2167393', 'numDocument': '41645490B'},
+                         {'id_prisma': '2166968', 'numDocument': '43230795H'},
+                         {'id_prisma': '2166671', 'numDocument': '73221885M'},
+                         {'id_prisma': '2166442', 'numDocument': '47929763V'},
+                         {'id_prisma': '2166329', 'numDocument': '23821903K'},
+                         {'id_prisma': '2166157', 'numDocument': '41600603C'},
+                         {'id_prisma': '2166145', 'numDocument': '48036709J'},
+                         {'id_prisma': '2166067', 'numDocument': '48270505Z'},
+                         {'id_prisma': '2166005', 'numDocument': '39406537Q'},
+                         {'id_prisma': '2165889', 'numDocument': '53866745D'},
+                         {'id_prisma': '2165617', 'numDocument': '49285361H'},
+                         {'id_prisma': '2165370', 'numDocument': '48138659G'},
+                         {'id_prisma': '2164977', 'numDocument': '43636007Q'},
+                         {'id_prisma': '2164887', 'numDocument': '48022181K'},
+                         {'id_prisma': '2164805', 'numDocument': '39391127Q'},
+                         {'id_prisma': '2164558', 'numDocument': '48097958J'},
+                         {'id_prisma': '2164458', 'numDocument': '43205827M'},
+                         {'id_prisma': '2164383', 'numDocument': '23821727Y'},
+                         {'id_prisma': '2164117', 'numDocument': '46494260J'},
+                         {'id_prisma': '2164001', 'numDocument': '45648881E'},
+                         {'id_prisma': '2163975', 'numDocument': '45790582C'},
+                         {'id_prisma': '2163779', 'numDocument': '23302009L'},
+                         {'id_prisma': '2163595', 'numDocument': '47894493Y'},
+                         {'id_prisma': '1953736', 'numDocument': 'Y0725916G'},
+                         {'id_prisma': '1920503', 'numDocument': '47852616N'},
+                         {'id_prisma': '1908655', 'numDocument': '47691604T'},
+                         {'id_prisma': '1908273', 'numDocument': '48103682X'},
+                         {'id_prisma': '1907783', 'numDocument': '39409440K'},
+                         {'id_prisma': '1907709', 'numDocument': '48235146Y'},
+                         {'id_prisma': '1907406', 'numDocument': '47276068M'},
+                         {'id_prisma': '1907372', 'numDocument': '45832949K'}]
+
+            from random import randint
+            test_user = list_test[randint(0, len(list_test) - 1)]
+            id_prisma = test_user['id_prisma']
+
+            datos_json = {
+                "codiExpedient": "303023",
+                "codiPrograma": "22500733",
+                "codiOferta": "3",
+                "titol": "lalalala",
+                "modalitat": "5",
+                "director": "6",
+                "departament": "7",
+                "numDocument": "53316254T",
+                "descripcio": "9",
+                "idiomaTreball": "10",
+                "propostaAmbitCooperacio": "11",
+                "tematicaAmbiental": "12",
+                "centre": "13",
+                "codirector": "14",
+                "empresa": "15",
+                "personaContacteEmpresa": "16",
+                "confidencial": "17",
+                "tipusAltaTFE": "I"
+            }
+
+            res_aplic = requests.put(bussoa_url + "%s" % id_prisma, headers={'apikey': bussoa_apikey}, auth=(bussoa_user, bussoa_pass), json=datos_json)
+            return res_aplic
--- genweb/tfemarket/browser/views.py
+++ genweb/tfemarket/browser/views.py
@@ -17,13 +17,12 @@ from genweb.theme.browser.views import _render_cachekey
 
 from genweb.tfemarket import _
 from genweb.tfemarket.content.offer import IOffer
-from genweb.tfemarket.controlpanel import ITfemarketSettings
+from genweb.tfemarket.controlpanel import ITfemarketSettings, IBUSSOASettings
 from genweb.tfemarket.interfaces import IGenwebTfemarketLayer
 from genweb.tfemarket.utils import getLdapExactUserData
 from genweb.tfemarket.utils import getLdapUserData
 from genweb.tfemarket.utils import LDAPSearch
 from genweb.tfemarket.utils import offerIsFromTheTeacher
-
 
 import json
 import pkg_resources
@@ -168,67 +167,172 @@ class getInfoCreateApplication(grok.View):
     grok.require('cmf.AddPortalContent')
     grok.layer(IGenwebTfemarketLayer)
 
-    def getIdPrisma(self, cn):
-        idPrisma = ()
-        user = LDAPSearch(self, cn)
-        if user['ok']:
-            keys = ['segmentation', 'unit', 'typology', 'idorigen']
-            data = []
-            for key in keys:
-                for ind, i in enumerate(user['content'][0][key]):
-                    try:
-                        data[ind].update({key: i})
-                    except IndexError:
-                        data.append({key: i})
-            for item in data:
-                if item['typology'] == 'EST':
-                    idPrisma = item['idorigen']
-                    return idPrisma
-        return ''
+    def getDadesStudent(self, cn):
+        registry = queryUtility(IRegistry)
+        bussoa_tool = registry.forInterface(IBUSSOASettings)
+        tfe_tool = registry.forInterface(ITfemarketSettings)
+        bussoa_url = bussoa_tool.bus_url
+        bussoa_user = bussoa_tool.bus_user
+        bussoa_pass = bussoa_tool.bus_password
+        bussoa_apikey = bussoa_tool.bus_apikey
+        tipus_alta = 'I' # tfe_tool.enroll_type
 
-    def render(self):
-        current = api.user.get_current()
-        user = getLdapExactUserData(current.id)
-        data = {}
-        if user and 'sn' in user:
-            data = {
+        student_data = {}
+        keys = ['segmentation', 'unit', 'typology', 'idorigen']
+        vinculacio = []
+        result = LDAPSearch(self, cn)
+        if result['ok']:
+            user = result['content'][0]
+            student_data = {
                 'offer_id': self.context.offer_id,
                 'offer_title': self.context.title,
-                'fullname': user['sn'],
-                'dni': user['DNIpassport'],
-                'email': user['mail'],
+                'fullname': user['fullname'],
+                'dni': user['dni'],
+                'email': user['email'],
             }
-
             if 'telephoneNumber' in user:
-                data.update({'phone': user['telephoneNumber']})
+                student_data.update({'phone': user['phone']})
 
-        registry = queryUtility(IRegistry)
-        tfe_tool = registry.forInterface(ITfemarketSettings)
-        id_prisma = self.getIdPrisma(current.id)
-        if id_prisma:
-            data.update({'idPrisma': id_prisma})
-            bussoa_url = tfe_tool.bus_url
-            bussoa_user = tfe_tool.bus_user
-            bussoa_pass = tfe_tool.bus_password
-            bussoa_apikey = tfe_tool.bus_apikey
-            tipus_alta = '53316254T'
-            id_prisma = '2708479'
-            res_data = requests.get(bussoa_url + "%s" % id_prisma + '?tipusAltaTFE=' + "%s" % (tipus_alta), headers={'apikey': bussoa_apikey}, auth=(bussoa_user, bussoa_pass))
+            # COMENTADO PARA DEMO
+
+            # for key in keys:
+            #     for ind, i in enumerate(user[key]):
+            #         try:
+            #             vinculacio[ind].update({key: i})
+            #         except IndexError:
+            #             vinculacio.append({key: i})
+
+            # isStudent = (True for item in vinculacio if item['typology'] == 'EST')
+            # if True not in isStudent:
+            #     self.context.plone_utils.addPortalMessage(_(u"No tens viculació d'ESTUDIANT"), 'error')
+            # else:
+            #     for item in vinculacio:
+            #         if item['typology'] == 'EST':
+            #             id_prisma = item['idorigen']
+            #             student_data.update({'idPrisma': id_prisma})
+            #             break
+
+            # ONLY FOR TRY #####################
+
+            list_test = [{'id_prisma': '2866124', 'numDocument': '44522242S'},
+                         {'id_prisma': '2708530', 'numDocument': '47405847H'},
+                         {'id_prisma': '2708479', 'numDocument': '41747970A'},
+                         {'id_prisma': '2707723', 'numDocument': '18079004S'},
+                         {'id_prisma': '2706798', 'numDocument': '48085070M'},
+                         {'id_prisma': '2554111', 'numDocument': '53870113L'},
+                         {'id_prisma': '2553255', 'numDocument': '39934092C'},
+                         {'id_prisma': '2553249', 'numDocument': '53835514N'},
+                         {'id_prisma': '2553092', 'numDocument': '26063484E'},
+                         {'id_prisma': '2552221', 'numDocument': '21798723J'},
+                         {'id_prisma': '2551839', 'numDocument': 'X2756193B'},
+                         {'id_prisma': '2551740', 'numDocument': '73461145L'},
+                         {'id_prisma': '2551700', 'numDocument': '41578237X'},
+                         {'id_prisma': '2551388', 'numDocument': '47238061V'},
+                         {'id_prisma': '2550963', 'numDocument': '48031179A'},
+                         {'id_prisma': '2550923', 'numDocument': '79272419K'},
+                         {'id_prisma': '2550789', 'numDocument': '53316254T'},
+                         {'id_prisma': '2438885', 'numDocument': 'Y3346939Q'},
+                         {'id_prisma': '2433865', 'numDocument': '70077461A'},
+                         {'id_prisma': '2407950', 'numDocument': '05315643K'},
+                         {'id_prisma': '2407902', 'numDocument': '41574784F'},
+                         {'id_prisma': '2401711', 'numDocument': '47322096X'},
+                         {'id_prisma': '2401541', 'numDocument': '77624311R'},
+                         {'id_prisma': '2400827', 'numDocument': '17765920F'},
+                         {'id_prisma': '2400653', 'numDocument': '47835848B'},
+                         {'id_prisma': '2400648', 'numDocument': '48032008G'},
+                         {'id_prisma': '2400208', 'numDocument': '39936041Z'},
+                         {'id_prisma': '2400053', 'numDocument': '48037883Z'},
+                         {'id_prisma': '2399504', 'numDocument': '25624813F'},
+                         {'id_prisma': '2399333', 'numDocument': '45126841J'},
+                         {'id_prisma': '2399207', 'numDocument': '47332956Z'},
+                         {'id_prisma': '2399205', 'numDocument': '41597001Y'},
+                         {'id_prisma': '2399187', 'numDocument': 'Y3541903D'},
+                         {'id_prisma': '2398864', 'numDocument': '48037052B'},
+                         {'id_prisma': '2398761', 'numDocument': '43575236B'},
+                         {'id_prisma': '2398743', 'numDocument': '05458237S'},
+                         {'id_prisma': '2398335', 'numDocument': '53292318F'},
+                         {'id_prisma': '2398310', 'numDocument': '48090627L'},
+                         {'id_prisma': '2398303', 'numDocument': '46414025W'},
+                         {'id_prisma': '2398292', 'numDocument': '21780110F'},
+                         {'id_prisma': '2398291', 'numDocument': '53869317M'},
+                         {'id_prisma': '2398187', 'numDocument': '48019958Y'},
+                         {'id_prisma': '2398145', 'numDocument': '47838860X'},
+                         {'id_prisma': '2398058', 'numDocument': '48099790M'},
+                         {'id_prisma': '2397841', 'numDocument': '23878574C'},
+                         {'id_prisma': '2397674', 'numDocument': '47592922B'},
+                         {'id_prisma': '2397654', 'numDocument': '43460008J'},
+                         {'id_prisma': '2397644', 'numDocument': '48026090C'},
+                         {'id_prisma': '2397485', 'numDocument': '43570423M'},
+                         {'id_prisma': '2381463', 'numDocument': '47838895E'},
+                         {'id_prisma': '2201379', 'numDocument': '41586362Q'},
+                         {'id_prisma': '2195877', 'numDocument': '49876030R'},
+                         {'id_prisma': '2184958', 'numDocument': '24412328J'},
+                         {'id_prisma': '2174310', 'numDocument': '41549964G'},
+                         {'id_prisma': '2174137', 'numDocument': '41583105W'},
+                         {'id_prisma': '2173869', 'numDocument': '47962264L'},
+                         {'id_prisma': '2173315', 'numDocument': '53399800X'},
+                         {'id_prisma': '2167398', 'numDocument': '41577897S'},
+                         {'id_prisma': '2167393', 'numDocument': '41645490B'},
+                         {'id_prisma': '2166968', 'numDocument': '43230795H'},
+                         {'id_prisma': '2166671', 'numDocument': '73221885M'},
+                         {'id_prisma': '2166442', 'numDocument': '47929763V'},
+                         {'id_prisma': '2166329', 'numDocument': '23821903K'},
+                         {'id_prisma': '2166157', 'numDocument': '41600603C'},
+                         {'id_prisma': '2166145', 'numDocument': '48036709J'},
+                         {'id_prisma': '2166067', 'numDocument': '48270505Z'},
+                         {'id_prisma': '2166005', 'numDocument': '39406537Q'},
+                         {'id_prisma': '2165889', 'numDocument': '53866745D'},
+                         {'id_prisma': '2165617', 'numDocument': '49285361H'},
+                         {'id_prisma': '2165370', 'numDocument': '48138659G'},
+                         {'id_prisma': '2164977', 'numDocument': '43636007Q'},
+                         {'id_prisma': '2164887', 'numDocument': '48022181K'},
+                         {'id_prisma': '2164805', 'numDocument': '39391127Q'},
+                         {'id_prisma': '2164558', 'numDocument': '48097958J'},
+                         {'id_prisma': '2164458', 'numDocument': '43205827M'},
+                         {'id_prisma': '2164383', 'numDocument': '23821727Y'},
+                         {'id_prisma': '2164117', 'numDocument': '46494260J'},
+                         {'id_prisma': '2164001', 'numDocument': '45648881E'},
+                         {'id_prisma': '2163975', 'numDocument': '45790582C'},
+                         {'id_prisma': '2163779', 'numDocument': '23302009L'},
+                         {'id_prisma': '2163595', 'numDocument': '47894493Y'},
+                         {'id_prisma': '1953736', 'numDocument': 'Y0725916G'},
+                         {'id_prisma': '1920503', 'numDocument': '47852616N'},
+                         {'id_prisma': '1908655', 'numDocument': '47691604T'},
+                         {'id_prisma': '1908273', 'numDocument': '48103682X'},
+                         {'id_prisma': '1907783', 'numDocument': '39409440K'},
+                         {'id_prisma': '1907709', 'numDocument': '48235146Y'},
+                         {'id_prisma': '1907406', 'numDocument': '47276068M'},
+                         {'id_prisma': '1907372', 'numDocument': '45832949K'}]
+
+            from random import randint
+            test_user = list_test[randint(0, len(list_test) - 1)]
+            id_prisma = test_user['id_prisma']
+            numDocument = test_user['numDocument']
+
+            res_data = requests.get(bussoa_url + "%s" % id_prisma + '?tipusAltaTFE=' + "%s" % tipus_alta + '&numDocument=' + "%s" % numDocument, headers={'apikey': bussoa_apikey}, auth=(bussoa_user, bussoa_pass))
 
             if res_data.ok:
-                student_data = res_data.json()
-                data.update({'llista_expedients': student_data['llistatExpedients']})
-                return json.dumps(data)
+                data = res_data.json()
+                num_expedients = data['llistatExpedients']
+
+                if num_expedients:
+                    student_data.update({'llista_expedients': num_expedients})
+                else:
+                    self.context.plone_utils.addPortalMessage(_(u"No tens número d'expedient a Prisma"), 'error')
 
             else:
                 status_code = res_data.status_code
                 reason = res_data.reason
-                self.context.plone_utils.addPortalMessage(_(u"PRISMA id not found."), 'error')
-
-                return (status_code, reason)
+                self.context.plone_utils.addPortalMessage(_(u"PRISMA id not found at PRISMA. %s" % (str(status_code) + ' ' + reason)), 'error')
 
         else:
-            return 'error'
+            self.context.plone_utils.addPortalMessage(_(u"Usuari no trobat en el Ldap"), 'error')
+        return student_data
+
+    def render(self):
+        current = api.user.get_current()
+        data = self.getDadesStudent(current)
+        return json.dumps(data)
 
 
 class resetCountOffers(grok.View):
@@ -240,8 +344,8 @@ class resetCountOffers(grok.View):
     def render(self):
         if 'confirm' in self.request.form:
             registry = queryUtility(IRegistry)
-            tfe_tool = registry.forInterface(ITfemarketSettings)
-            tfe_tool.count_offers = 0
+            bussoa_tool = registry.forInterface(ITfemarketSettings)
+            bussoa_tool.count_offers = 0
             transaction.commit()
             self.request.response.redirect(self.context.absolute_url() + "/tfemarket-settings#fieldsetlegend-2")
         else:
