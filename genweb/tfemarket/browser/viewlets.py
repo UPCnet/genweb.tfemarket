4e10e8f14ff112b2b1d4effeb64352dac825a62a
iago.lopez <iago.lopez@upcnet.es>
2018-09-28 13:05:35 +0200

    Eliminar viewlet sin uso

--- genweb/tfemarket/browser/viewlets.py
+++ genweb/tfemarket/browser/viewlets.py
@@ -2,9 +2,7 @@
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from five import grok
-from plone.app.layout.viewlets.interfaces import IAboveContentTitle
 from plone.app.layout.viewlets.interfaces import IPortalTop
-from zope.interface import Interface
 from zope.security import checkPermission
 
 from genweb.theme.browser.viewlets import gwPersonalBarViewlet
@@ -25,30 +23,3 @@ class genwebTfemarketPersonalBarViewlet(gwPersonalBarViewlet):
 
     def canManageTFE(self):
         return checkPermission("genweb.tfemarket.controlpanel", self)
-
-
-class infoTfemarket(grok.Viewlet):
-    grok.viewletmanager(IAboveContentTitle)
-    grok.context(Interface)
-    grok.name('genweb.tfemarket.info')
-    grok.template('infoTfemarket')
-    grok.layer(IGenwebTfemarketLayer)
-
-    def viewInfoTfeMarket(self):
-        validTypes = ['genweb.tfemarket.market',
-                      'genweb.tfemarket.offer',
-                      'genweb.tfemarket.application']
-        if self.context.portal_type in validTypes:
-            return True
-        return False
-
-    def urlInfo(self):
-        url = ""
-        portal_type = self.context.portal_type
-        if portal_type == 'genweb.tfemarket.market':
-            url = self.context.absolute_url() + "/manual-dus"
-        elif portal_type == 'genweb.tfemarket.offer':
-            url = self.context.getParentNode().absolute_url() + "/manual-dus"
-        elif portal_type == 'genweb.tfemarket.application':
-            url = self.context.getParentNode().getParentNode().absolute_url() + "/manual-dus"
-        return url
