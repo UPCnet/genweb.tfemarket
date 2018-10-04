78c591d15279c5d8c058779ad9ff5df6b302489a
Corina Riba <corina.riba@upcnet.es>
2018-10-04 08:36:53 +0200

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages, setup
 import os
 
-version = '1.0a2.dev0'
+version = '1.0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
