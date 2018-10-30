677de182a5497ab482633b85f3859bbd2ee46a01
Corina Riba <corina.riba@upcnet.es>
2018-10-30 12:19:08 +0100

    Back to development: 1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages, setup
 import os
 
-version = '1.2'
+version = '1.3.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
