d66180d3213fa8647aef0fdb5192974f5a445772
Iago López Fernández <iago.lopez@upcnet.es>
2019-05-23 07:42:22 +0200

    Carga de trabajo: Quitar valor por defecto y añadirlo a la descripción.

--- genweb/tfemarket/content/offer.py
+++ genweb/tfemarket/content/offer.py
@@ -344,8 +344,7 @@ class IOffer(form.Schema):
 
     workload = schema.TextLine(
         title=_(u'offer_workload'),
-        description=_(u'Un crèdit ECTS equival a 25 hores de treball'),
-        default=_(u'La càrrega de treball s\'adptarà als crèdits de la titulació.'),
+        description=_(u'Un crèdit ECTS equival a 25 hores de treball. La càrrega de treball s\'adaptarà als crèdits de la titulació.'),
         required=True,
     )
 
