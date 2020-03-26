48fdbbb198aae9352e1cd09e10ccaf2d12683012
Iago López Fernández <iago.lopez@upcnet.es>
2020-03-26 10:23:53 +0100

    Back to development: 1.5.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages, setup
 import os
 
-version = '1.5.6'
+version = '1.5.7.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
